d5f305b2e71bd455bdfc8da47a97bc5783d501eb
chetramaaseri <90443950+chetramaaseri@users.noreply.github.com>
2023-05-04 12:02:58 +0530

    added packages
    included in head & script and in index .php ...added index .html to block direct access

---
+++ constant/head.php
@@ -0,0 +1,1 @@
+<link rel="stylesheet" href="./vendor/twbs/bootstrap/dist/css/bootstrap.min.css">
---
+++ constant/script.php
@@ -0,0 +1,2 @@
+<script src="./vendor/twbs/bootstrap/dist/js/bootstrap.min.js"></script>
+<script src="./vendor/components/jquery/jquery.min.js"></script>
